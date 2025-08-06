fe21c57e7b479fcebf5fe4721e7d057774b20839
Nithusikan001 <e19266@eng.pdn.ac.lk>
2025-08-07 02:21:54 +0530

    feat: :seedling: Lambda function intro code file is uploaded.

--- Basics/python-lambda-fn.py
+++ Basics/python-lambda-fn.py
@@ -13,17 +13,6 @@ print(z)
 
 print(f"Type of z: {type(z)}")
 print(f"Type of y: {type(y)}")
-
-# The power of lambda is better shown when you use them as an anonymous function inside another function.
-# Say you have a function definition that takes one argument, and that argument will be multiplied with an unknown number:
-
-def myfunc(n):
-    return lambda a : a * n
-
-mydoubler = myfunc(2)
-print(mydoubler(10))
-
-mytripler = myfunc(3)
 print(mytripler(10))
 
 # pass args in single line
